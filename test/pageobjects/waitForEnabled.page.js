54f86f86f6fdd038433cceac8b31f4ec8bc31cae
LaLouange <Louangetryphene01@gmail.com>
2025-07-02 09:39:13 +0200

    3rd commit

--- test/pageobjects/waitForEnabled.page.js
+++ test/pageobjects/waitForEnabled.page.js
@@ -1,13 +1,27 @@
 class WaitForEnabled {
     get enableButton() {
-        return $('#input-example button')
+        return $('//button[contains(text(), "Enable")]')
     }
     get inputEnabledField() {
         return $('#input-example input')
     }
+    get disabledbutton() {
+        return $('//button[contains(text(), "Disable")]')
+    }
+    get pageButton() {
+        return $('#checkbox-example button')
+    }
+    clickPageButton() {
+        this.pageButton.waitForDisplayed()
+        this.pageButton.click()
+    }
     async clickEnableButton() {
         await this.enableButton.waitForDisplayed()
         await this.enableButton.click()
+    }
+    async clickDisabledButton() {
+        await this.disabledbutton.waitForDisplayed()
+        await this.disabledbutton.click()
     }
 }
 export default new WaitForEnabled();
--- test/specs/waitForEnabled.test.js
+++ test/specs/waitForEnabled.test.js
@@ -1,17 +1,34 @@
 import assert from "assert"
 import WaitForEnabled from "../pageobjects/waitForEnabled.page"
 
-
 describe('Wait For Enabled', () => {
     it('should wait for enabled', async () => {
         await browser.url('https://the-internet.herokuapp.com/dynamic_controls')
         await WaitForEnabled.clickEnableButton()
-        await WaitForEnabled.inputEnabledField.waitForEnabled(4000)
+        await WaitForEnabled.inputEnabledField.waitForEnabled({ timeout: 4000 })
         assert.equal(await WaitForEnabled.inputEnabledField.isEnabled(), true)
     })
     it('should wait for the input field to be disabled', async () => {
-        await WaitForEnabled.clickEnableButton()
-        await WaitForEnabled.inputEnabledField.waitForEnabled(4000, true)
-        assert.equal(await WaitForEnabled.inputEnabledField.isEnabled(), true)
+        await WaitForEnabled.clickDisabledButton()
+        await WaitForEnabled.inputEnabledField.waitForEnabled({ reverse: true })
+        assert.equal(await WaitForEnabled.inputEnabledField.isEnabled(), false)
+    })
+})
+describe.only('WaitUntil', () => {
+    it('should wait until the button text changes to Add', async () => {
+        await browser.url('https://the-internet.herokuapp.com/dynamic_controls')
+        await WaitForEnabled.clickPageButton()
+        browser.waitUntil(async () => {
+            return await WaitForEnabled.pageButton.getText() === 'Add'
+        }, 
+        6000, 'Expect button text to change')
+    })
+    it('should wait until the button text changes to Remove', async () => {
+        await WaitForEnabled.clickPageButton()
+        browser.waitUntil(async () => {
+            return await WaitForEnabled.pageButton.getText() === 'Remove'
+        }, 
+        6000, 'Expect button text to change')
+        assert.equal(await WaitForEnabled.pageButton.getText(), 'Remove')
     })
 })
